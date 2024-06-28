59501d0a1b4ce0e38aba74faa939bec11275c00b
tingxuan0911 <candy521927@yahoo.com.tw>
2024-06-28 14:37:19 +0800

    1

--- src/components/header.vue
+++ src/components/header.vue
@@ -7,23 +7,23 @@ const device = inject("device");
 <template>
   <div>
     <div class="p-5 mb-32">
-      <div class="relative w-full h-screen">
+      <div class="relative w-full h-screen text-white text-center p-5">
         <!-- 使用 ::before 伪元素显示背景图片 -->
-        <div class="absolute inset-0 bg-cover bg-center"
-             :style="{ backgroundImage: `url(${headerPic})` }"></div>
+        <div
+          class="header inset-0 bg-cover bg-center"
+          :style="{ backgroundImage: `url(${headerPic})` }"
+        ></div>
 
-        <div class="flex flex-col justify-center items-center h-full text-white text-center">
-          <div class="text-xs">2024</div>
-          <div class="text-2xl font-bold">We’re getting married</div>
-          <div class="flex justify-between pt-3 mt-5 border-t border-white">
-            <div>Jay & Gabrielle</div>
-            <div>DEC.17</div>
-          </div>
-          <div class="absolute bottom-[10%] right-0 text-end text-3xl">
-            please join us
-            <br />and
-            <br />celebrate together
-          </div>
+        <div class="text-xs">2024</div>
+        <div class="text-2xl font-bold">We’re getting married</div>
+        <div class="flex justify-between pt-3 mt-5 border-t border-white">
+          <div>Jay & Gabrielle</div>
+          <div>DEC.17</div>
+        </div>
+        <div class="absolute bottom-[10%] right-0 text-end text-3xl">
+          please join us
+          <br />and
+          <br />celebrate togethe
         </div>
       </div>
     </div>
@@ -31,22 +31,15 @@ const device = inject("device");
 </template>
 
 <style scoped>
-html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-/* 背景图片的样式设置 */
-.absolute {
+.header {
   position: absolute;
   z-index: -1;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-position: center;
-  background-image: url(${headerPic});
+  /* width: 100%; */
+  /* height: 100%; */
+  /* background-size: cover; */
+  /* background-position: center; */
+  /* background-image: url(${headerPic}); */
 }
 </style>
