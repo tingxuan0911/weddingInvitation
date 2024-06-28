15aedef889b26ce3766982891b6f1aa4aa10958f
tingxuan0911 <candy521927@yahoo.com.tw>
2024-06-28 13:40:24 +0800

    1

--- src/components/header.vue
+++ src/components/header.vue
@@ -6,25 +6,26 @@ import headerPic from "@/img/header.jpg";
   <div>
     <div class="p-5 mb-32">
       <div
-        class="header relative w-full h-dvh p-5 bg-cover bg-center bg-no-repeat bg-fixed text-white text-center"
-            :style="{ 'background-image': `url(${headerPic}) `}"
-        
-      >
-    
-      </div>
+        class="header relative w-full h-dvh p-5 bg-cover bg-center bg-fixed text-white text-center"
+        :style="{ 'background-image':` url(${headerPic}) `}"
+      ></div>
     </div>
   </div>
 </template>
 <style scoped>
-  /* .header::before{
-    contain: '';
-    position: fixed;
-    z-index: -1;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-size: cover;
-    background-color: transparent;
-  } */
+html {
+  height: 100%;
+}
+/* .header::before {
+  contain: "";
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-size: cover;
+  background-color: transparent;
+  background-image: url(./img/header.jpg);
+} */
 </style>
